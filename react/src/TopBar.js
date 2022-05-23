ce5daed324c5b338aba3cae09f135317ee00bb35
rosemanjake <roseman.jake@gmail.com>
2022-05-23 22:06:03 +0100

    Rearranging menus, adding dropdowns

--- react/src/TopBar.js
+++ react/src/TopBar.js
@@ -5,6 +5,13 @@ function TopBar(props) {
   const [showtoc, setShowToC] = useState(false);
   const [showvdrop, setVDrop] = useState(false); // show version dropdown
   const [showoptions, setShowOptions] = useState(false); // show version dropdown
+  const [showversionoptions, setShowVersionOptions] = useState(false)
+  const [versionhover, setVersionHover] = useState(false)
+  const [showchapteroptions, setShowChapterOptions] = useState(false)
+  const [chapterhover, setChapterHover] = useState(false)
+
+  const f = obj => Object.fromEntries(Object.entries(obj).map(a => a.reverse()))
+  const flippedversions = f(props.versions)
 
   return (
     <>
@@ -14,6 +21,10 @@ function TopBar(props) {
           <div className="maintitle">BEOWULF</div>
           <div className="maintitleline"></div>
           <ChapterTitle chapter={props.chapter} chaptertitles={props.chaptertitles}/>
+        </div>
+        <div className="topbuttons">
+          <TopDropDown type={"Chapters"} showdropdown={showversionoptions} setShowDropDown={setShowVersionOptions} setOther={setShowChapterOptions} hoverstate={versionhover} setHoverState={setVersionHover} chaptertitles={props.chaptertitles} onChapterChange={props.onChapterChange} currchapter={props.chapter}/>         
+          <TopDropDown type={"Translations"} showdropdown={showchapteroptions} setShowDropDown={setShowChapterOptions} setOther={setShowVersionOptions} hoverstate={chapterhover} setHoverState={setChapterHover} flippedversions={flippedversions} versions={props.versions} onVersionChange={props.onVersionChange} currversion={props.version}/>    
         </div>
         <div className="hamburgercontainer" onClick={() => setShowOptions(!showoptions)}>
           <svg viewBox="-55 0 100 80" width="40" height="40" className="hamburger">
@@ -33,77 +44,96 @@ function TopBar(props) {
 
 // Good read on handling input: https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs
 
+function TopDropDown(props){
+
+  return (
+    <>
+    <div className="dropdowncontainer" 
+      onClick={() => 
+        {
+          props.setShowDropDown(!props.showdropdown);
+          props.setHoverState(false);
+          props.setOther(false);
+        }
+      }
+      onMouseOver={() => props.setHoverState(true)} 
+      onMouseOut={() => props.setHoverState(false)}
+    >
+    <div className='dropdownbuttoncontainer'>
+      <div className='dropdown'
+        style={(props.hoverstate) ? {color: "var(--gold)"} : {}}
+      >{props.type}</div>
+      <div 
+        className={(props.showdropdown) ? 'optionsarrowactive' : 'optionsarrow'}
+        style={(props.hoverstate) ? {borderLeft: "7px solid var(--gold)"} : {}}
+      ></div>
+    </div>
+    {props.type === "Translations" &&
+      <VersionItems showdropdown={props.showdropdown} flippedversions={props.flippedversions} onVersionChange={props.onVersionChange} versions={props.versions} currversion={props.currversion}/>
+    }
+    {props.type === 'Chapters' &&
+      <ChapterItems chaptertitles={props.chaptertitles} showdropdown={props.showdropdown} onChapterChange={props.onChapterChange} currchapter={props.currchapter}/>
+    }
+    </div>  
+    </>
+  )
+
+}
+
+function VersionItems(props){
+  return(
+    <>
+    {props.showdropdown &&
+      <>
+        <div className="menuversions">
+        {
+          Object.keys(props.versions)
+            .map(version =>
+              <div className={(version == props.flippedversions[props.currversion]) ? 'activeversionentry' : 'versionentry'} key={version + "dropdown"} onClick={() => props.onVersionChange(props.versions[version])}>{version}</div>
+            )
+        }
+        </div>
+      </>
+    }
+    </>
+  )
+}
+
+function ChapterItems(props){
+  return(
+    <>
+    <div className='bigdropdown'>
+    {props.showdropdown &&
+      <>
+        <div className="menuversions">
+        {
+          Object.keys(props.chaptertitles)
+            .map(chapter =>
+              <div className={(chapter == props.currchapter) ? 'activechapterentry' : 'chapterentry'} key={chapter + "dropdown"} onClick={() => props.onChapterChange(chapter)}>{chapter}</div>
+            )
+        }
+        </div>
+      </>
+    }
+    </div>
+    </>
+  )
+}
+
 function Options(props){
-  const f = obj => Object.fromEntries(Object.entries(obj).map(a => a.reverse()))
-  const versions = f(props.versions)
-
-  const [showchapteroptions, setShowChapterOptions] = useState(false)
-  const [showversionoptions, setShowVersionOptions] = useState(false)
-  const [showabout, setShowAbout] = useState(false)
-
-  const [chapterselection, setChapterSelection] = useState(props.chapter)
-
-  const handleChapterSelection = () => {
-    const selection = parseInt(chapterselection.replace(/\s*/gms,""))
-    if (selection > 0 && selection < 44 && selection != props.chapter){
-      props.onChapterChange(selection)
-    }
-    else{
-      alert("Please insert number between 1 and 43.")
-    }
-  }
-
-  // I should break this down into more components, I know
   return(
     <>
     <div className="optionscontainer">
     <div className="options">
-      <div className="optionsheadingcontainer" onClick={() => setShowChapterOptions(!showchapteroptions)}>
-        <div className='optionsheading'>Chapters</div>
-        <div className={(showchapteroptions) ? 'optionsarrowactive' : 'optionsarrow'}></div>
-      </div>
-      {showchapteroptions &&
-        <>
-        <div className="optionstext">Current chapter: {props.chapter} of 43</div>
-        <div className='navigationcontainer'>
-          <div>Select Chapter:</div>
-          <input type="text" name="chapter" className='chapterinput' onInput={e => setChapterSelection(e.target.value)}/>       
-        </div>
-        <div className='navigationbutton' onClick={handleChapterSelection}>Go</div>
-        </>
-      }     
-      <div className="optionsheadingcontainer" onClick={() => setShowVersionOptions(!showversionoptions)}>
-        <div className='optionsheading'>Versions</div>
-        <div className={(showversionoptions) ? 'optionsarrowactive' : 'optionsarrow'}></div>
-      </div>
-      {showversionoptions &&
-        <>
-          <div className="menuversions">
-          {
-            Object.keys(props.versions)
-              .map(version =>
-                <div className={(version == versions[props.version]) ? 'activeversionentry' : 'versionentry'} key={version + "dropdown"} onClick={() => props.onVersionChange(props.versions[version])}>{version}</div>
-              )
-          }
-          </div>
-        </>
-      }
-      <div className="optionsheadingcontainer" onClick={() => setShowAbout(!showabout)}>
-        <div className='optionsheading'>About</div>
-        <div className={(showabout) ? 'optionsarrowactive' : 'optionsarrow'}></div>
-      </div>
-      {showabout &&
-        <>
-        <div className="optionstext">This website was designed and built by <a href="https://www.jakeroseman.com">Jake Roseman.</a></div>
-        <div className="optionstext">All texts are in the public domain and sourced from project Gutenberg:</div>
-        <ul>
-          <li>Gummere, Francis. (1910). Beowulf. <a href="https://www.gutenberg.org/ebooks/981">Gutenberg</a></li>
-          <li>Hall, Lessie. (1892). Beowulf: An Anglo-Saxon Epic Poem. <a href="https://www.gutenberg.org/ebooks/16328">Gutenberg</a></li>
-          <li>Kirtlan, Ernest J. B. (1914). The Story of Beowulf. <a href="https://www.gutenberg.org/ebooks/50742">Gutenberg</a></li>
-          <li>Morris, William. (1895). The Tale of Beowulf, Sometime King of the Folk of the Weder Geats. <a href="https://www.gutenberg.org/ebooks/20431">Gutenberg</a></li>
-        </ul>
-        </>
-      }
+      <div className="donatebutton" onClick={() => window.open("https://www.paypal.com/donate/?hosted_button_id=TNN9K9X56KVPQ")}>Donate</div> 
+      <div className="optionstext">This website was designed and built by <a href="https://www.jakeroseman.com">Jake Roseman.</a></div>
+      <div className="optionstext">All texts are in the public domain and sourced from project Gutenberg:</div>
+      <ul>
+        <li>Gummere, Francis. (1910). Beowulf. <a href="https://www.gutenberg.org/ebooks/981">Gutenberg</a></li>
+        <li>Hall, Lessie. (1892). Beowulf: An Anglo-Saxon Epic Poem. <a href="https://www.gutenberg.org/ebooks/16328">Gutenberg</a></li>
+        <li>Kirtlan, Ernest J. B. (1914). The Story of Beowulf. <a href="https://www.gutenberg.org/ebooks/50742">Gutenberg</a></li>
+        <li>Morris, William. (1895). The Tale of Beowulf, Sometime King of the Folk of the Weder Geats. <a href="https://www.gutenberg.org/ebooks/20431">Gutenberg</a></li>
+      </ul>   
     </div>
     </div>
     </>
